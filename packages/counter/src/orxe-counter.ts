d0807a198e23c99eafde02ff6d0397036da1939f
ninad <ninad.virkar@coditas.com>
2020-09-18 20:21:48 +0530

    basic component

--- packages/counter/src/orxe-counter.ts
+++ packages/counter/src/orxe-counter.ts
@@ -1,16 +1,83 @@
-import { html, customElement, LitElement } from 'lit-element';
+import { html, customElement, LitElement, property } from 'lit-element';
 import styles from './counter-css';
 
 @customElement('orxe-counter')
 export default class OrxeCounter extends LitElement {
+
+  constructor() {
+    super();
+    this.value = parseInt(this.getAttribute("value")!.toString())
+    this.min = parseInt(this.getAttribute("min")!.toString())
+    this.max = parseInt(this.getAttribute("max")!.toString())
+  }
+
   /**
-   * Implement `render` to define a template for button element.
+   * @memberof OrxeCounter
+   * this property denotes the actual value
    */
+  @property({ type: Number })
+  value = 0;
+
+  /**
+ * @memberof OrxeCounter
+ * this property denotes the minimum value
+ */
+  @property({ type: Number })
+  min = 0;
+
+  /**
+ * @memberof OrxeCounter
+ * this property denotes the maximum value
+ */
+  @property({ type: Number })
+  max = 0;
+
+  /**
+  * Implement `render` to define a template for button element.
+  */
   render() {
     return html`
-      <p>Hello, Welcome to lit Elements</p>
+    <button @click="${this.decrement}" id="decrement"> - </button>
+       <div>${this.value}</div>
+    <button @click="${this.increment}" id="increment"> + </button>
     `;
   }
+  /**
+   * This method handles decrement
+   */
+  decrement() {
+    this.value--;
+    this.handleEdgeCase();
+  }
+
+  /**
+   * This method handles increment
+   */
+  increment() {
+    this.value++
+    this.handleEdgeCase()
+  }
+  /**
+   * This method handles edge case
+   */
+
+  handleEdgeCase() {
+    let incrementButton = (<HTMLInputElement>this.shadowRoot!.getElementById("increment"))
+    let decrementButton = (<HTMLInputElement>this.shadowRoot!.getElementById("decrement"))
+    if (this.value == this.max) {
+      incrementButton.disabled = true;
+    }
+    else {
+      incrementButton.disabled = false;
+    }
+    if (this.value == this.min) {
+      decrementButton.disabled = true;
+    }
+    else {
+      decrementButton.disabled = false;
+    }
+  }
+
 
   /**
    *  Getting styles from components custom scss file
